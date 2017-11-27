89a2572a3e48d07918958085b326c99f95bdedc6
buzzhaha <boming.fan@gmail.com>
2017-11-27 15:17:19 -0800

    hw3-finish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,23 +28,21 @@ var reset = function () {
 
 document.onkeyup = function (event) {
 
-    var html =
-    "<h1>Let's Play the Psychic Game</h1>" +
-    "<p>Wins: " + win + "</p>" +
-    "<p>Losses: " + loss + "</p>" +
-    "<p>Guess Left: " + guessLeft + "</p>" +
-    "<p>You Guesses so far: " + guessHistory + "</p>";    
+    console.log(document.onkeyup);
 
-
+// minus the guessleft by 1
     guessLeft--;
 
+// whenever user press the key
     var userGuess = event.key;
 
+// Computer guess random letters 
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+// update the array
     guessHistory.push(userGuess)
 
-    // in 9 attempts, if user guess matches the computerguess, user win and reset, otherwise, keep going until guessing right or running out of guesses and user loses
+// in 9 attempts, if user guess matches the computerguess, user win and reset, otherwise, keep going until guessing right or running out of guesses and user loses
    
         if (guessLeft > 0) {
             if (userGuess === computerGuess) {
@@ -56,12 +54,16 @@ document.onkeyup = function (event) {
                 loss++;
                 reset();
         }
+          
+// Write text in html    
+      var html =
+        "<h1>Let's Play the Psychic Game</h1>" +
+        "<p>Wins: " + win + "</p>" +
+        "<p>Losses: " + loss + "</p>" +
+        "<p>Guess Left: " + guessLeft + "</p>" +
+        "<p>You Guesses so far: " + guessHistory + "</p>";  
         
-         
-
-     
-
-    document.querySelector("#game").innerHTML = html;
+        document.querySelector("#game").innerHTML = html;
 
 }
 
